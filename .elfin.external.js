9469964a39b8e210f73a5cea307ca036076fd7db
rengarxiao <rengarxiao@tencent.com>
2021-03-05 15:11:18 +0800

    feat: init

---
+++ .elfin.external.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-algorithm.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: true,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-commander.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-dataStructure.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-demo.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: true,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-eslint.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-finance.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: true,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-utils.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-vscode.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+    {
+        sshAddress: 'git@github.com:XyyF/elfinUI.git',                   /* git ssh地址 */
+        // branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+]
